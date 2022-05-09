d21d275f0be327970bb2f986af18577727408c7a
Gridy <reality023@naver.com>
2022-05-10 08:12:38 +0900

    Master Class #2.3 Complete

--- src/App.js
+++ src/App.js
@@ -4,21 +4,20 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${props => props.bgColor};
-  width: 100px;
-  height: 100px;
+const Input = styled.input.attrs({required: true, minLength: 10})`
+  background-color: tomato;
+
 `;
 
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="whitesmoke" />
+    <Father as="header">
+      <input type="text" required />
+      <input type="text" required />
+      <input type="text" required />
+      <input type="text" required />
+      <input type="text" required />
     </Father>
   );
 }

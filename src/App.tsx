f0a38a46133f0f0be89e0b778464fabb23ef5975
Gridy <reality023@naver.com>
2022-05-16 22:08:52 +0900

    리액트 마스터 클래스 #3.6 끝

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
+import React, { useState } from "react";
 import  styled from "styled-components";
 import Circle from "./Circle";
 
+const Container = styled.div`
+  background-color: ${props => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${props => props.theme.textColor};
+`
+
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: {value}
+    } = event;
+    setValue(value);
+  }
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Hello", value);
+  };
   return (
-    <div>
-      <Circle bgColor="teal" borderColor="white" />
-      <Circle text="im here" bgColor="tomato" />
-    </div>
+    <Container>
+      <H1>protected</H1>
+    </Container>
   );
 }
 

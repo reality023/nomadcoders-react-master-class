c71fad9f808175135801486e2cefed2ecb98d7f6
Gridy <reality023@naver.com>
2022-05-17 22:13:17 +0900

    리액트 마스터 클래스 #3.7 끝

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,24 @@ const H1 = styled.h1`
   color: ${props => props.theme.textColor};
 `
 
+interface DummyProps {
+  text: string;
+  active ?: boolean;
+}
+
+function Dummy({ text, active = false }: DummyProps) {
+  return <h1>{text}</h1>;
+}
+
 function App() {
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: {value}
-    } = event;
-    setValue(value);
-  }
-  const onSubmit = (event:React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("Hello", value);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+
   };
   return (
     <Container>
       <H1>protected</H1>
+      <Dummy active text="hello"/> // active는 active={true}와 같다
+      <button onClick={onClick}>click me</button>
     </Container>
   );
 }

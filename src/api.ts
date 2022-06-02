81d50cd447dcb35748ece6d9545470a54d5c2cdf
Gridy <reality023@naver.com>
2022-06-02 23:59:19 +0900

    #4.14 Save

--- src/api.ts
+++ src/api.ts
@@ -18,3 +18,11 @@ export function fetchCoinTickers(coinId:string) {
         return response.json();
     });
 }
+
+export function fetchCoinHistory(coinId: string) {
+    const endDate = Math.floor(Date.now() / 1000);
+    const startDate = endDate - 60 * 60 * 24 * 3;
+    return fetch(`${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`).then((response) => {
+        return response.json();
+    });
+}
--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,93 @@
-function Chart() {
-    return <h1>Chart</h1>;
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import ApexChart from "react-apexcharts";
+import { fetchCoinHistory } from "../api";
+
+interface ChartProps {
+    coinId: string;
+}
+
+interface IHistorical {
+    time_open: string;
+    time_close: string;
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+    volume: number;
+    market_cap: number;
+}
+
+function Chart({ coinId }: ChartProps) {
+    const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () => (
+        fetchCoinHistory(coinId)
+    ));
+
+    return <div>
+        {
+        isLoading ? 
+        "Loading chart..." : 
+        <ApexChart
+            type="line"
+            series={[
+                {
+                    name: "Price",
+                    data: data?.map((price) => price.close) as number[],
+                    // data?.map((price) => price.close) ?? []
+                },
+            ]}
+            options={{
+                theme: {
+                    mode: "dark"
+                },
+                chart: {
+                    height: 300,
+                    width: 500,
+                    toolbar: {
+                        show: false,
+                    },
+                    background: "transparent",
+                },
+                grid: {
+                    show: false,
+                },
+                stroke: {
+                    curve: "smooth",
+                    width: 4,
+                },
+                yaxis: {
+                    show: false,
+                },
+                xaxis: {
+                    type: "datetime",
+                    labels: {
+                        show: false,
+                    },
+                    axisTicks: {
+                        show: false,
+                    },
+                    axisBorder: {
+                        show: false,
+                    },
+                    categories: data?.map((price) => price.time_close),
+                },
+                fill: {
+                    type: "gradient",
+                    gradient: {
+                        gradientToColors: ["#0be881"],
+                        stops: [0, 100]
+                    }
+                },
+                colors: ["#0fbcf9"],
+                tooltip: {
+                    y: {
+                        formatter: (value) => `${value.toFixed(3)}`
+                    }
+                }
+            }}
+        />
+        }
+    </div>;
 }
 
 export default Chart;

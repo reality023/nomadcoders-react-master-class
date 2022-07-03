a8f1f22041ad13a9cabffb031396ff92ea5cfd76
Gridy <reality023@naver.com>
2022-07-04 06:19:43 +0900

    #6.2 Drag and Drop part One Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+
+// React 18 react-beautiful-dnd Error Fix
+// npm install --save-dev react-beautiful-dnd --legacy-peer-deps
+// npm install --save-dev --force react-beautiful-dnd
 
 function App() {
+	const onDragEnd = () => {};
   return (
-		<div>
-		</div>
+		<DragDropContext onDragEnd={onDragEnd}>
+			<div>
+				<Droppable droppableId="one">{() =>
+					(
+					<ul>
+						<Draggable draggableId="first" index={0}>
+							{() => <li>Hello</li>}
+						</Draggable>
+						<Draggable draggableId="second" index={1}>
+							{() => <li>Hello</li>}
+						</Draggable>
+					</ul>
+					)
+				}</Droppable>
+			</div>
+		</DragDropContext>
 	)
 }
 

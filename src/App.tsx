1b100a6e1295eb69ee3a164bbea47cb24ae794ed
Gridy <reality023@naver.com>
2022-07-04 05:47:49 +0900

    #6.1 Set Selectors Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import React from "react";
+import { useRecoilState } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
+
 function App() {
+	const [minutes, setMinutes] = useRecoilState(minuteState);
+	const [hours, setHours] = useRecoilState(hourSelector);
+	const onMinutesChange = (event : React.FormEvent<HTMLInputElement>) => {
+		setMinutes(+event.currentTarget.value); // 문자를 숫자로
+	};
+
+	const onHoursChange = (event : React.FormEvent<HTMLInputElement>) => {
+		setHours(+event.currentTarget.value);
+	}
   return (
 		<div>
+			<input value={minutes} onChange={onMinutesChange} type="number" placeholder="Minutes" />
+			<input value={hours} onChange={onHoursChange} type="number" placeholder="Hours" />
 		</div>
 	)
 }

c43d601061b7a08a1243ec978a26939445a6ec4e
Gridy <reality023@naver.com>
2022-07-04 06:32:11 +0900

    #6.3 Drag and Drop part Two Complete

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,28 @@ function App() {
   return (
 		<DragDropContext onDragEnd={onDragEnd}>
 			<div>
-				<Droppable droppableId="one">{() =>
-					(
-					<ul>
+				<Droppable droppableId="one">{(magic) => (
+					<ul ref={magic.innerRef} {...magic.droppableProps}>
 						<Draggable draggableId="first" index={0}>
-							{() => <li>Hello</li>}
+							{(magic) => 
+							(
+								<li ref={magic.innerRef} {...magic.draggableProps} >
+									<span {...magic.dragHandleProps}>SELECT</span>
+									One
+								</li>
+							)
+							}
 						</Draggable>
 						<Draggable draggableId="second" index={1}>
-							{() => <li>Hello</li>}
+							{(magic) => (
+								<li ref={magic.innerRef} {...magic.draggableProps}>
+									<span {...magic.dragHandleProps}>SELECT</span>
+									Two
+								</li>
+							)}
 						</Draggable>
 					</ul>
-					)
-				}</Droppable>
+				)}</Droppable>
 			</div>
 		</DragDropContext>
 	)
